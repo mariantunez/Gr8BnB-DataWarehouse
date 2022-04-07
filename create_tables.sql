39586e4e50af95137758b8d95e14af2f9829adda
Marisol Antunez <mariantunez@users.noreply.github.com>
2022-04-07 09:15:34 -0700

    Upload cloverDX

---
+++ create_tables.sql
@@ -0,0 +1,28 @@
+--  Create Data Warehouse Schema 
+CREATE SCHEMA IF NOT EXISTS Gr8BnBDataWarehouse;
+USE Gr8BnBDataWarehouse;
+
+-- Drop Tables
+DROP TABLE IF EXISTS CrimeData;
+DROP TABLE IF EXISTS CulturalSpaces;
+DROP TABLE IF EXISTS HomePriceIndex;
+
+
+-- Create Table CrimeData
+-- CREATE TABLE CrimeData (
+-- 	
+-- );
+
+
+
+# Create Table CulturalSpaces
+-- CREATE TABLE CulturalSpaces (
+-- 	
+-- );
+
+
+
+# Create HomePriceIndex
+-- CREATE TABLE HomePriceIndex (
+--     
+-- );
